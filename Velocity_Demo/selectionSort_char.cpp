0f93e13f88c02de47f2795390361e74ac6022227
Abhishek Kumar <64278461+abhishek210597@users.noreply.github.com>
2023-02-01 12:07:02 +0530

    Create selectionSort_char.cpp
    selectionSort_char

---
+++ Velocity_Demo/selectionSort_char.cpp
@@ -0,0 +1,36 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<vector>
+using namespace std;
+void selectionSort(char fruit[][60],int n){
+    
+    for(int i=0; i<n-1; i++){
+        
+        int min_idx=i;
+        for(int j=i+1; j<n; j++){
+            if(strcmp(fruit[min_idx], fruit[j])>0){
+                min_idx = j;
+            }
+        }
+        
+        if(i!=min_idx){
+            swap(fruit[i], fruit[min_idx])
+            char temp[60]
+            strcpy()
+        }
+    }
+    return;
+}
+
+
+int main() {
+    char fruit[][60]={"papaya", "lime", "watermelon", "apple", "mango", "kiwi"}
+    int n =sizeof(fruit)/sizeof(fruit[0]);
+    selectionSort(fruit, n);
+    for(int i=0; i<n; i++){
+        cout<<fruit[i]<<" ";
+    }cout<<endl;
+    
+    
+    return 0;
+}
